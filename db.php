f2234e856b83f6c1ca8980fce5d0276440ca65a5
JonnhyMC <132869088+JonnhyMC@users.noreply.github.com>
2025-12-11 00:34:38 -0600

    Add files via upload

--- db.php
+++ db.php
@@ -1,14 +1,15 @@
 <?php
-$DB_HOST = 'localhost';
-$DB_USER = 'root';
-$DB_PASS = '1104jonathan';   // la misma que pusiste en phpMyAdmin
-$DB_NAME = 'lacocada';
 
-$mysqli = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
+$mysqli = new mysqli(
+    getenv("MYSQLHOST"),
+    getenv("MYSQLUSER"),
+    getenv("MYSQLPASSWORD"),
+    getenv("MYSQLDATABASE"),
+    intval(getenv("MYSQLPORT"))
+);
 
-if ($mysqli->connect_error) {
-    die('Error de conexión a la base de datos: ' . $mysqli->connect_error);
+if ($mysqli->connect_errno) {
+    die("Error al conectar con MySQL: " . $mysqli->connect_error);
 }
 
-$mysqli->set_charset('utf8mb4');
 ?>
